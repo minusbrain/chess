de885a90c87eda20a428fe23c38619747bb8da7d
Andreas Evers <aevers@gmx.de>
2022-07-26 23:41:23 +0200

    Add getter methods to Move class in order to read out details
    Add ability to read out start field, end field and chess piece

--- src/move.h
+++ src/move.h
@@ -17,8 +17,12 @@ class Move {
         : _piece(piece), _startField{startLine, startRow}, _endField{endLine, endRow}, _mods(mods) {}
 
     void addModifier(MoveModifier mod) { _mods.insert(mod); }
-    std::set<MoveModifier> getModifiers() { return _mods; }
-    bool hasModifier(MoveModifier mod) { return base::find(_mods, mod) != _mods.end(); }
+    std::set<MoveModifier> getModifiers() const { return _mods; }
+    bool hasModifier(MoveModifier mod) const { return base::find(_mods, mod) != _mods.end(); }
+
+    ChessPiece getChessPiece() const { return _piece; }
+    ChessField getStartFiled() const { return _startField; }
+    ChessField getEndFiled() const { return _endField; }
 
    private:
     ChessPiece _piece;
