df3a0559c8c3cac1400eba1b04d420265b95e233
Andreas Evers <aevers@gmx.de>
2022-07-31 11:11:45 +0200

    Make run_chess use info whos turn it is instead assuming white

--- src/main.cpp
+++ src/main.cpp
@@ -5,23 +5,26 @@
 #include "move.h"
 #include "move_debug.h"
 #include "rules.h"
+#include "types.h"
 
 int main(int argc, char** argv) {
     std::string fen;
     if (argc > 1) {
         fen.assign(argv[1]);
+    } else {
+        fen = "8/5k2/3p4/1p1Pp2p/pP2Pp1P/P4P1K/8/8 b - - 99 50";
     }
 
     Board fenBoard = BoardFactory::creatBoardFromFEN(fen);
 
-    fmt::print("\nFEN String {}", fen);
+    fmt::print("\nFEN String {}\n", fen);
     fmt::print("FEN Board:\n{:b}\n", fenBoard);
 
-    auto validMovesWhite = ChessRules::getAllValidMoves(fenBoard, Color::WHITE);
+    auto validMoves = ChessRules::getAllValidMoves(fenBoard, fenBoard.whosTurnIsIt());
 
-    fmt::print("Valid moves for White:\n");
-    for (auto validMoveWhite : validMovesWhite) {
-        fmt::print("Move: {}\n", validMoveWhite);
+    fmt::print("Valid moves for {}:\n", (fenBoard.whosTurnIsIt() == Color::WHITE ? "white" : "black"));
+    for (auto validMove : validMoves) {
+        fmt::print("Move: {}\n", validMove);
     }
 
     return 0;
