fa36ccde311b52617440438b14b70d6369710db6
Andreas Evers <aevers@gmx.de>
2022-08-02 16:16:18 +0200

    Implement Castling rule checks

--- test/test_rules.cpp
+++ test/test_rules.cpp
@@ -19,12 +19,17 @@ void EXPECT_CONTAINS(T item, C container) {
         << "Container " << container << " does not contain " << item;
 }
 
+template <class T, class C>
+void EXPECT_DOESNOT_CONTAIN(T item, C container) {
+    EXPECT_EQ(container.end(), std::find(container.begin(), container.end(), item)) << "Container " << container << " contains " << item;
+}
+
 int getNumberOfMovesForPiece(std::vector<Move> moves, ChessPiece piece) {
     return base::count_if(moves, [&piece](const Move& move) { return move.getChessPiece() == piece; });
 }
 
 TEST(TestChessRules, PawnPositions_PawnOnBaseLine_FindAllPossibleWhiteMoves) {
-    Board board = BoardFactory::creatBoardFromFEN("k7/p7/8/8/8/8/P7/K7 w - -");
+    Board board = BoardFactory::createBoardFromFEN("k7/p7/8/8/8/8/P7/K7 w - -");
 
     auto moves = ChessRules::getAllValidMoves(board);
 
@@ -34,7 +39,7 @@ TEST(TestChessRules, PawnPositions_PawnOnBaseLine_FindAllPossibleWhiteMoves) {
 }
 
 TEST(TestChessRules, PawnPositions_PawnOnBaseLine_FindAllPossibleBlackMoves) {
-    Board board = BoardFactory::creatBoardFromFEN("k7/p7/8/8/8/8/P7/K7 b - -");
+    Board board = BoardFactory::createBoardFromFEN("k7/p7/8/8/8/8/P7/K7 b - -");
 
     auto moves = ChessRules::getAllValidMoves(board);
 
@@ -44,7 +49,7 @@ TEST(TestChessRules, PawnPositions_PawnOnBaseLine_FindAllPossibleBlackMoves) {
 }
 
 TEST(TestChessRules, PawnPositions_PawnOnOtherLine_FindAllPossibleWhiteMoves) {
-    Board board = BoardFactory::creatBoardFromFEN("k7/8/p7/8/8/P7/8/K7 w - -");
+    Board board = BoardFactory::createBoardFromFEN("k7/8/p7/8/8/P7/8/K7 w - -");
 
     auto moves = ChessRules::getAllValidMoves(board);
 
@@ -53,7 +58,7 @@ TEST(TestChessRules, PawnPositions_PawnOnOtherLine_FindAllPossibleWhiteMoves) {
 }
 
 TEST(TestChessRules, PawnPositions_PawnOnOtherLine_FindAllPossibleBlackMoves) {
-    Board board = BoardFactory::creatBoardFromFEN("k7/8/p7/8/8/P7/8/K7 b - -");
+    Board board = BoardFactory::createBoardFromFEN("k7/8/p7/8/8/P7/8/K7 b - -");
 
     auto moves = ChessRules::getAllValidMoves(board);
 
@@ -62,7 +67,7 @@ TEST(TestChessRules, PawnPositions_PawnOnOtherLine_FindAllPossibleBlackMoves) {
 }
 
 TEST(TestChessRules, PawnPositions_PawnBlocked_FindAllPossibleWhiteMoves) {
-    Board board = BoardFactory::creatBoardFromFEN("k7/8/8/p7/P7/8/8/K7 w - -");
+    Board board = BoardFactory::createBoardFromFEN("k7/8/8/p7/P7/8/8/K7 w - -");
 
     auto moves = ChessRules::getAllValidMoves(board);
 
@@ -70,7 +75,7 @@ TEST(TestChessRules, PawnPositions_PawnBlocked_FindAllPossibleWhiteMoves) {
 }
 
 TEST(TestChessRules, PawnPositions_PawnBlocked_FindAllPossibleBlackMoves) {
-    Board board = BoardFactory::creatBoardFromFEN("k7/8/8/p7/P7/8/8/K7 b - -");
+    Board board = BoardFactory::createBoardFromFEN("k7/8/8/p7/P7/8/8/K7 b - -");
 
     auto moves = ChessRules::getAllValidMoves(board);
 
@@ -78,7 +83,7 @@ TEST(TestChessRules, PawnPositions_PawnBlocked_FindAllPossibleBlackMoves) {
 }
 
 TEST(TestChessRules, PawnPositions_PawnCanCapture_FindAllPossibleWhiteMoves) {
-    Board board = BoardFactory::creatBoardFromFEN("k7/8/8/1p6/P7/8/8/K7 w - -");
+    Board board = BoardFactory::createBoardFromFEN("k7/8/8/1p6/P7/8/8/K7 w - -");
 
     auto moves = ChessRules::getAllValidMoves(board);
 
@@ -88,7 +93,7 @@ TEST(TestChessRules, PawnPositions_PawnCanCapture_FindAllPossibleWhiteMoves) {
 }
 
 TEST(TestChessRules, PawnPositions_PawnCanCapture_FindAllPossibleBlackMoves) {
-    Board board = BoardFactory::creatBoardFromFEN("k7/8/8/1p6/P7/8/8/K7 b - -");
+    Board board = BoardFactory::createBoardFromFEN("k7/8/8/1p6/P7/8/8/K7 b - -");
 
     auto moves = ChessRules::getAllValidMoves(board);
 
@@ -98,7 +103,7 @@ TEST(TestChessRules, PawnPositions_PawnCanCapture_FindAllPossibleBlackMoves) {
 }
 
 TEST(TestChessRules, PawnPositions_PawnCanPromote_FindAllPossibleWhiteMoves) {
-    Board board = BoardFactory::creatBoardFromFEN("k7/3P4/8/8/8/8/3p4/K7 w - -");
+    Board board = BoardFactory::createBoardFromFEN("k7/3P4/8/8/8/8/3p4/K7 w - -");
 
     auto moves = ChessRules::getAllValidMoves(board);
 
@@ -110,7 +115,7 @@ TEST(TestChessRules, PawnPositions_PawnCanPromote_FindAllPossibleWhiteMoves) {
 }
 
 TEST(TestChessRules, PawnPositions_PawnCanPromote_FindAllPossibleBlackMoves) {
-    Board board = BoardFactory::creatBoardFromFEN("k7/3P4/8/8/8/8/3p4/K7 b - -");
+    Board board = BoardFactory::createBoardFromFEN("k7/3P4/8/8/8/8/3p4/K7 b - -");
 
     auto moves = ChessRules::getAllValidMoves(board);
 
@@ -122,7 +127,7 @@ TEST(TestChessRules, PawnPositions_PawnCanPromote_FindAllPossibleBlackMoves) {
 }
 
 TEST(TestChessRules, PawnPositions_PawnCanPromoteByCapture_FindAllPossibleWhiteMoves) {
-    Board board = BoardFactory::creatBoardFromFEN("k1rB4/3P4/8/8/8/8/3p4/K1Rb4 w - -");
+    Board board = BoardFactory::createBoardFromFEN("k1rB4/3P4/8/8/8/8/3p4/K1Rb4 w - -");
 
     auto moves = ChessRules::getAllValidMoves(board);
 
@@ -134,7 +139,7 @@ TEST(TestChessRules, PawnPositions_PawnCanPromoteByCapture_FindAllPossibleWhiteM
 }
 
 TEST(TestChessRules, PawnPositions_PawnCanPromoteByCapture_FindAllPossibleBlackMoves) {
-    Board board = BoardFactory::creatBoardFromFEN("k1rB4/3P4/8/8/8/8/3p4/K1Rb4 b - -");
+    Board board = BoardFactory::createBoardFromFEN("k1rB4/3P4/8/8/8/8/3p4/K1Rb4 b - -");
 
     auto moves = ChessRules::getAllValidMoves(board);
 
@@ -146,11 +151,29 @@ TEST(TestChessRules, PawnPositions_PawnCanPromoteByCapture_FindAllPossibleBlackM
 }
 
 TEST(TestChessRules, PawnPositions_PawnCanCaptureEnPassant_FindAllPossibleWhiteMoves) {
-    Board board = BoardFactory::creatBoardFromFEN("k7/8/8/8/3Pp3/8/8/K7 b - d3");
+    Board board = BoardFactory::createBoardFromFEN("k7/8/8/8/3Pp3/8/8/K7 b - d3");
 
     auto moves = ChessRules::getAllValidMoves(board);
 
     EXPECT_CONTAINS(Move{{Color::BLACK, Piece::PAWN}, {E, 4}, {E, 3}}, moves);
     EXPECT_CONTAINS(Move{{Color::BLACK, Piece::PAWN}, {E, 4}, {D, 3}, {MoveModifier::CAPTURE, MoveModifier::EN_PASSANT}}, moves);
     EXPECT_EQ(2, getNumberOfMovesForPiece(moves, {Color::BLACK, Piece::PAWN}));
+}
+
+TEST(TestChessRules, Castling_WhiteKingPathCoveredByEnemy_ExpectNoCastlingOptions) {
+    Board board = BoardFactory::createBoardFromFEN("1k1r1r2/8/8/8/8/8/8/R3K2R w QK -");
+
+    auto moves = ChessRules::getAllValidMoves(board);
+
+    EXPECT_DOESNOT_CONTAIN(Move{{Color::WHITE, Piece::KING}, {E, 1}, {G, 1}, {MoveModifier::CASTLING_SHORT}}, moves);
+    EXPECT_DOESNOT_CONTAIN(Move{{Color::WHITE, Piece::KING}, {E, 1}, {C, 1}, {MoveModifier::CASTLING_LONG}}, moves);
+}
+
+TEST(TestChessRules, Castling_BlackKingPathCoveredByEnemy_ExpectNoCastlingOptions) {
+    Board board = BoardFactory::createBoardFromFEN("r3k2r/8/8/8/8/8/8/3RKR2 b qk -");
+
+    auto moves = ChessRules::getAllValidMoves(board);
+
+    EXPECT_DOESNOT_CONTAIN(Move{{Color::BLACK, Piece::KING}, {E, 8}, {G, 8}, {MoveModifier::CASTLING_SHORT}}, moves);
+    EXPECT_DOESNOT_CONTAIN(Move{{Color::BLACK, Piece::KING}, {E, 8}, {C, 8}, {MoveModifier::CASTLING_LONG}}, moves);
 }
