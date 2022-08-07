45e1b0d9a391791836c29cc55883ee95e750483d
Andreas Evers <aevers@gmx.de>
2022-08-07 23:40:32 +0200

    Rename fenBoard to board in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -14,16 +14,16 @@ int main(int, char**) {
     std::string fen;
 
     while (std::getline(std::cin, fen)) {
-        Board fenBoard = BoardFactory::createBoardFromFEN(fen);
-        std::cout << "FEN String " << fen << fenBoard;
+        Board board = BoardFactory::createBoardFromFEN(fen);
+        std::cout << "FEN String " << fen << board;
 
-        Legality legality = ChessRules::determineBoardPositionLegality(fenBoard);
+        Legality legality = ChessRules::determineBoardPositionLegality(board);
         std::cout << "Board position is " << (legality == Legality::LEGAL ? "legal" : "illegal") << "\n";
 
         if (legality == Legality::LEGAL) {
-            auto validMoves = ChessRules::getAllValidMoves(fenBoard);
+            auto validMoves = ChessRules::getAllValidMoves(board);
 
-            std::cout << "Valid moves for " << (fenBoard.whosTurnIsIt() == Color::WHITE ? "white" : "black") << ": " << validMoves << "\n";
+            std::cout << "Valid moves for " << (board.whosTurnIsIt() == Color::WHITE ? "white" : "black") << ": " << validMoves << "\n";
         }
     }
 
